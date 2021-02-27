b3d55a1f01bd8f5220ffc1767159d246f3f84a69
Omar Branez <omar.branez@gmail.com>
2021-02-27 14:12:56 -0500

    commit tree f3996b83a264ea1fb8e7d6b17c3460d761223022

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,20 @@
-class Student 
+require_relative "./user.rb"
+class Student < User
+
+    
+   # @knowledge = []
+
+    def initialize
+        @knowledge = [] # this is an instance variable just for each student's knowledge
+        # not to be confused with the constant in Teacher
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
